30b2d5fdcf9f64646abd76644f6a0e820cc6b909
Jfolmer <113907783+Jfolmer@users.noreply.github.com>
2026-03-10 09:10:57 +0100

    Moved some stuff around to folders

---
+++ entities/resources/Resource.gd
@@ -0,0 +1,18 @@
+extends Entity
+
+var amount: int
+var maxAmount: int
+
+func harvest():
+    if amount > 0:
+        amount -= 1
+        print ("Resource harvested. Remaing in the node", amount)
+    else:
+        deplete():
+
+func deplete():
+    print("resource is empty")
+
+func regen():
+    if amount < maxAmount
+        amount += 1
