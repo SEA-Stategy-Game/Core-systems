2f89b6c003cc5d5c0355997f9d8f6b11ebbe5d75
Flávia Silva Pezoti <flaviapezoti@gmail.com>
2026-05-31 10:56:09 +0200

    bug fixes

--- Logic/Notifications/RedisNotificationReceiver.gd
+++ Logic/Notifications/RedisNotificationReceiver.gd
@@ -22,7 +22,10 @@ func _on_redis_message(channel: String, message: String) -> void:
 	var plan = JSON.parse_string(message)
 	if typeof(plan) == TYPE_DICTIONARY:
 		var p_id = str(plan.get("player_id", ""))
-		var u_ids = plan.get("unit_ids", [])
+		var raw_u_ids = plan.get("unit_ids", [])
+		var u_ids = []
+		for id in raw_u_ids:
+			u_ids.append(str(id))
 		plan_notified.emit(game_id, p_id, u_ids)
 	else:
 		push_error("RedisNotificationReceiver: Invalid JSON plan payload from PubSub.")
